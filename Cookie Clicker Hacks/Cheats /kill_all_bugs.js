5ce92e4db178cca6054f08a46182ecea60b157bf
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2024-12-09 11:28:34 -0500

    Create kill_all_bugs.js

---
+++ Cookie Clicker Hacks/Cheats /kill_all_bugs.js
@@ -0,0 +1,5 @@
+(function() {
+    // Kill all active bugs instantly
+    Game.bugLimit = 0; // Set bug limit to 0, which effectively kills all bugs
+    alert("All bugs have been instantly killed!");
+})();
