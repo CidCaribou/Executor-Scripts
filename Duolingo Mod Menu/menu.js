9d38d7d7e5dbdc49ce19c5627bc5437d5572a190
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2024-12-11 17:07:00 -0500

    Update menu.js

--- Duolingo Mod Menu/menu.js
+++ Duolingo Mod Menu/menu.js
@@ -1,5 +1,5 @@
 (function() {
-alert("You can get banned use at your own risk.\n\nMenu By Cid\n\nCheats Made by rxzyx on github");    
+alert("You can get banned use at your own risk.\n\nMenu By Wasd\n\nCheats Made by rxzyx on github");    
 
     const menu = document.createElement('div');
     menu.style.position = 'fixed';
