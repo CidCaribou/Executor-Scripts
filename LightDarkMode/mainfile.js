6441e37969e852a0a6ff550a51bf4ab7f8c08cbe
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2024-12-21 15:26:36 -0500

    Update mainfile.js

--- LightDarkMode/mainfile.js
+++ LightDarkMode/mainfile.js
@@ -11,7 +11,7 @@
 
     async function loadFiles() {
         // Fetch HTML, CSS, and JS content
-        const htmlContent = await fetchFile('https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@refs/heads/main/LightDarkMode/indexupdated.html');
+        const htmlContent = await fetchFile('https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@main/LightDarkMode/index.html');
         const cssContent = await fetchFile('https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@main/LightDarkMode/style.css');
         const jsContent = await fetchFile('https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@main/LightDarkMode/script.js');
 
