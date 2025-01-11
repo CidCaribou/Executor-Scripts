1913d4241ae34ae9660bebe8307e0f259c19929e
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-01-11 10:40:02 -0500

    Create Stain_Of_Paint.js

---
+++ Custom Cursors/Cursors/Stain_Of_Paint.js
@@ -0,0 +1,71 @@
+(function () {
+
+  const normalCursorUrl = "https://cdn.custom-cursor.com/db/7600/32/stain-of-paint-cursor.png"; 
+  const pointerCursorUrl = "https://cdn.custom-cursor.com/db/7599/32/stain-of-paint-pointer.png"; 
+  const textCursorUrl = "https://cdn.custom-cursor.com/db/7599/32/stain-of-paint-pointer.png"; 
+
+  function applyCustomCursors() {
+
+    var id = window.setTimeout(function () {}, 0);
+    while (id--) {
+      window.clearTimeout(id);
+    }
+
+    doMouse();
+
+    async function doMouse() {
+
+      document.querySelectorAll("*").forEach(function (node3) {
+        if (window.getComputedStyle(node3).cursor === "pointer") {
+          node3.setAttribute("cursor", "pointer");
+        }
+      });
+
+      document.querySelectorAll('*:not([cursor="pointer"])').forEach(function (node) {
+        if (canTypeInElement(node)) {
+          node.style.cursor = `url(${textCursorUrl}), text`; 
+        } else {
+          node.style.cursor = `url(${normalCursorUrl}), default`; 
+        }
+      });
+
+      document.querySelectorAll('*[cursor="pointer"]').forEach(function (node2) {
+        node2.style.cursor = `url(${pointerCursorUrl}), pointer`; 
+      });
+
+      await sleep(20);
+      doMouse(); 
+    }
+  }
+
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  function resetCursors() {
+    document.querySelectorAll("*").forEach(function (node) {
+      node.style.cursor = "default";
+    });
+  }
+
+  window.resetCursors = resetCursors;
+
+  applyCustomCursors();
+
+  function canTypeInElement(element) {
+    if (
+      (element instanceof HTMLInputElement && element.type !== "hidden") ||
+      element instanceof HTMLTextAreaElement ||
+      element.contentEditable === "true"
+    ) {
+      return true;
+    }
+    if (element instanceof HTMLSelectElement || element instanceof HTMLOptionElement) {
+      return true;
+    }
+    if (element instanceof HTMLButtonElement || (element.tagName.toLowerCase() === "button" && element.getAttribute("type") === "submit")) {
+      return true;
+    }
+    return false;
+  }
+})();
