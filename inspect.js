4cce4ce53a78906ff062ce15b3f715a521eb9ca6
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-01-21 16:42:25 -0500

    Update inspect.js

--- inspect.js
+++ inspect.js
@@ -1,7 +1,21 @@
-if (document.body.contentEditable !== 'true') {
+const swalScript = document.createElement('script');
+swalScript.src = 'https://cdn.jsdelivr.net/npm/sweetalert2@11';
+document.head.appendChild(swalScript);
+
+swalScript.onload = () => {
+  if (document.body.contentEditable !== 'true') {
     document.body.contentEditable = 'true';
-    alert('Inspect is ON');
-} else {
+    Swal.fire({
+      title: "Inspect Toggled",
+      text: "You Can Edit The Website Now.",
+      icon: "success"
+    });
+  } else {
     document.body.contentEditable = 'false';
-    alert('Inspect is OFF');
-}
+    Swal.fire({
+      title: "Inspect Disabled",
+      text: "You Can No Longer Edit The Website",
+      icon: "warning"
+    });
+  }
+};
