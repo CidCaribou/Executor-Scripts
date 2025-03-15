415cbf054b5cf1e85fc32cfede720abedf148750
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-03-15 17:41:24 -0400

    Create Clear-IndexDB.js

---
+++ Clear-IndexDB.js
@@ -0,0 +1,37 @@
+javascript:(function(){
+    let swalScript = document.createElement('script');
+    swalScript.src = "https://cdn.jsdelivr.net/npm/sweetalert2@11";
+    swalScript.onload = function() {
+        showConfirmation();
+    };
+    document.head.appendChild(swalScript);
+
+    function showConfirmation() {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "This will clear your IndexedDB storage. Some offline app data may be lost.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Yes, clear IndexedDB!",
+            cancelButtonText: "No, keep my data"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                clearIndexedDB();
+            } else {
+                Swal.fire("Operation Canceled", "Your IndexedDB was not cleared.", "info");
+            }
+        });
+    }
+
+    function clearIndexedDB() {
+        if ('indexedDB' in window) {
+            indexedDB.databases().then((databases) => {
+                databases.forEach((db) => indexedDB.deleteDatabase(db.name));
+            });
+        }
+
+        Swal.fire("IndexedDB Cleared!", "Your IndexedDB storage has been successfully cleared.", "success");
+    }
+})();
