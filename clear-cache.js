37a875c18e37ca7654a39c44a700a4310486ab79
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-03-15 17:31:49 -0400

    Create clear-cache.js

---
+++ clear-cache.js
@@ -0,0 +1,39 @@
+javascript:(function(){
+    let swalScript = document.createElement('script');
+    swalScript.src = "https://cdn.jsdelivr.net/npm/sweetalert2@11";
+    swalScript.onload = function() {
+        showConfirmation();
+    };
+    document.head.appendChild(swalScript);
+
+    function showConfirmation() {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "This will clear your browser's cache.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Yes, clear cache!",
+            cancelButtonText: "No, keep my cache"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                clearCache();
+            } else {
+                Swal.fire("Operation Canceled", "Your cache was not cleared.", "warning");
+            }
+        });
+    }
+
+    function clearCache() {
+        if ('caches' in window) {
+            caches.keys().then((cacheNames) => {
+                cacheNames.forEach((cacheName) => {
+                    caches.delete(cacheName);
+                });
+            });
+        }
+
+        Swal.fire("Cache Cleared!", "Your browser cache has been successfully cleared.", "success");
+    }
+})();
