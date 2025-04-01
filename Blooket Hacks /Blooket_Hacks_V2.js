c2e2cd9f1dcd186d15916bcf07e73dd894872b67
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-04-01 19:02:37 -0400

    Update Blooket_Hacks_V2.js

--- Blooket Hacks /Blooket_Hacks_V2.js
+++ Blooket Hacks /Blooket_Hacks_V2.js
@@ -1,5 +1,4 @@
-//Blooket Hacks V2 Has Moved Find It Here
-//This is the offical blooket cheats made by the offical owners
+//This is the offical blooket cheats made by the offical owners give them a star on github if you like what they do
 fetch('https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@main/Blooket%20Hacks%20/Blooket_Hacks_V2.js')
     .then(response => response.text())  
     .then(scriptText => eval(scriptText))
