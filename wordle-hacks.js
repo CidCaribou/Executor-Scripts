6137abcafe26f0409248105f3130c0468ca846f9
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-05-09 16:15:15 -0400

    Update wordle-hacks.js

--- wordle-hacks.js
+++ wordle-hacks.js
@@ -2,18 +2,9 @@ javascript: (function () {
   if (location.host == 'www.nytimes.com') {
     const date = new Date(),
       year = date.getFullYear(),
-      month = date.getMonth() + 1,
-      day = date.getDate(),
-      formattedMonth = month.toString().length === 1 ? '0' + month : month,
-      formattedDay = day.toString().length === 1 ? '0' + day : day,
-      url =
-        'https://www.nytimes.com/svc/wordle/v2/' +
-        year +
-        '-' +
-        formattedMonth +
-        '-' +
-        formattedDay +
-        '.json';
+      month = (date.getMonth() + 1).toString().padStart(2, '0'),
+      day = date.getDate().toString().padStart(2, '0'),
+      url = `https://www.nytimes.com/svc/wordle/v2/${year}-${month}-${day}.json`;
 
     fetch(url)
       .then((response) => response.json())
