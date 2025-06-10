4e7735b246a1ae1291a25d3147a1cfebd5b93354
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-06-10 17:47:06 -0400

    Update x-ray.js

--- x-ray/x-ray.js
+++ x-ray/x-ray.js
@@ -1,6 +1,6 @@
 javascript: (function() {
     var script = document.createElement('script');
-    script.src = 'https://x-ray-goggles.mouse.org/webxray.js';
+    script.src = 'https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@main/x-ray/x-ray-goggles.js';
     script.className = 'webxray';
     script.setAttribute('data-lang', 'en-US');
     script.setAttribute('data-baseuri', 'https://x-ray-goggles.mouse.org');
