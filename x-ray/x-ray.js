8f309ece450719ee1491955c6080c095929e821f
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-06-10 17:49:35 -0400

    Delete x-ray directory

--- x-ray/x-ray.js
+++
@@ -1,8 +0,0 @@
-javascript: (function() {
-    var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@main/x-ray/x-ray-goggles.js';
-    script.className = 'webxray';
-    script.setAttribute('data-lang', 'en-US');
-    script.setAttribute('data-baseuri', 'https://x-ray-goggles.mouse.org');
-    document.body.appendChild(script);
-}())
