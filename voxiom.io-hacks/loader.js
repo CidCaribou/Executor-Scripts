72523a21144ecf6483b46d38ab4a1c824c97e2cd
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-09-23 09:43:26 -0400

    Update loader.js

--- voxiom.io-hacks/loader.js
+++ voxiom.io-hacks/loader.js
@@ -1,31 +1,20 @@
-function loadScript(src, callback) {
+const loadScript = (src, callback) => {
   const script = document.createElement('script');
   script.src = src;
-  script.onload = callback; 
-  script.onerror = function () {
-    console.error(`Failed to load script: ${src}`);
-  };
+  script.onload = callback;
+  script.onerror = () => console.error(`Failed to load script: ${src}`);
   document.head.appendChild(script);
-}
+};
 
-loadScript('https://cdn.jsdelivr.net/npm/lil-gui@0.19.2/dist/lil-gui.umd.min.js', function () {
-  console.log('lil-gui loaded');
-  loadScript('https://unpkg.com/three@0.150.0/build/three.min.js', function () {
-    console.log('three.js loaded');
-  loadScript('https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@main/voxiom.io-hacks/hack.js', function () {
-    console.log('hacks loaded');
-
-    if (typeof swal === 'undefined') {
-      loadScript('https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js', function () {
-        console.log('SweetAlert2 loaded');
-        showSuccessAlert();
-      });
-    } else {
-      console.log('swal is already defined');
-      showSuccessAlert();
-    }
-  });
-});
+loadScript('https://cdn.jsdelivr.net/npm/lil-gui@0.19.2/dist/lil-gui.umd.min.js', () =>
+  loadScript('https://unpkg.com/three@0.150.0/build/three.min.js', () =>
+    loadScript('https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@main/voxiom.io-hacks/hack.js', () =>
+      loadScript('https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js', () =>
+        showSuccessAlert()
+      )
+    )
+  )
+);
 
 function showSuccessAlert() {
   Swal.fire({
