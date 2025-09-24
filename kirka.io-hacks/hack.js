ce49370792537804c7deb2b9b75a90e8a9f1c1c1
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-09-24 17:02:01 -0400

    Update hack.js

--- kirka.io-hacks/hack.js
+++ kirka.io-hacks/hack.js
@@ -1,8 +1,7 @@
 const scripts = [
   "https://cdn.jsdelivr.net/npm/sweetalert2@11", 
   "https://cdn.jsdelivr.net/npm/three@0.155.0/build/three.min.js", 
-  "https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@main/kirka.io-hacks/silent.js",
-  "https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@main/kirka.io-hacks/wallhack.js"
+  "https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@main/kirka.io-hacks/hacks.js",
 ];
 
 function loadScript(url) {
