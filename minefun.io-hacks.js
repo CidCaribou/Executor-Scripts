57711855187fbb65f9a0434516b165024abf7e85
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-10-31 10:43:21 -0400

    Update minefun.io-hacks.js

--- minefun.io-hacks.js
+++ minefun.io-hacks.js
@@ -3,6 +3,14 @@
 // author crackbob
 // homepage https://github.com/crackbob/Minebuns
 // downloadURL https://update.greasyfork.org/scripts/537529/MineFunreg.user.js
+
+Swal.fire({
+    title: 'Success!',
+    text: 'Hack loaded successfully! Press , to open the gui.',
+    icon: 'success',
+    confirmButtonText: 'Okay'
+  });
+}
  
 (() => {
     "use strict";
