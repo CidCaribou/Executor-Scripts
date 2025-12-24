311e33de770d1aa270f4822cbdd22e014957474c
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-12-24 09:55:32 -0500

    Update hook.js

--- ev-dot-io/hook.js
+++ ev-dot-io/hook.js
@@ -1,15 +1,25 @@
-(function() {
+(function(){
     'use strict';
 
     console.log('[Scene Hook] Hooking WeakMap...');
+    window.worldScene=null;
 
-    window.worldScene = null;
+    const failed=localStorage.getItem('ev.io-hook-failed')==='true';
 
-    WeakMap.prototype.set = new Proxy(WeakMap.prototype.set, {
-        apply(target, thisArgs, [object]) {
-            if (object.type === 'Scene' && object.children.length > 4) {
-                window.worldScene = object;
-                console.log('[Scene Hook] SCENE FOUND!', window.worldScene);
+    WeakMap.prototype.set=new Proxy(WeakMap.prototype.set,{
+        apply(t,a,[o]){
+            if(o.type==='Scene'&&o.children.length>4){
+                window.worldScene=o;
+                console.log('[Scene Hook] SCENE FOUND!',o);
+
+                if(failed){
+                    Swal.fire({
+                        icon:'success',
+                        title:'Hook successful',
+                        text:'Scene was hooked successfully after a failed attempt.'
+                    });
+                    localStorage.setItem('ev.io-hook-failed','false');
+                }
             }
             return Reflect.apply(...arguments);
         }
